84e229e045693a26c105105efa143cea31b1fddd
Krystian Kaczorek <115180141+starrkTheDev@users.noreply.github.com>
2024-05-28 14:09:06 +0200

    Added Bars component

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,15 +8,28 @@ import LogoutButton from './buttons/LogoutButton';
 import MyPostsButton from './buttons/MyPostsButton';
 import { useNavigate } from 'react-router-dom';
 import { useLanguage } from '../files/LanguageContext';
+import Bars from './buttons/Bars';
 
 const Header = () => {
 
     const [isAddForm, setIsAddForm] = useState(false);
     const { switchLanguage, language } = useLanguage();
+    const [modal, setModal] = useState(false);
 
     const handleLanguageSwitch = (newLanguage) => {
         switchLanguage(newLanguage);
     };
+
+    const modalHandler = () => {
+        setModal(true);
+        if(modal) {
+            setModal(false);
+        }
+    }
+
+    const modalDisabler = () => {
+        setModal(false);
+    }
     
     const secondPageEnable = () => {
         if (!isAddForm) {
@@ -32,46 +45,55 @@ const Header = () => {
     const token = localStorage.getItem('token');
 
     return (
-        <div className={classes.header}>
-            <div className={classes.wrapper}>
-                <div className={classes.firstLine}>
-                    <div onClick={() => navigate('/')}
-                        className={classes.name}>
-                            zamieszkaj.pl
+        <div>
+            <div className={classes.header}>
+                <div className={classes.wrapper}>
+                    <div className={classes.firstLine}>
+                        <div onClick={() => navigate('/')}
+                            className={classes.name}>
+                                zamieszkaj.pl
+                        </div>
+                        <div className={classes.languageContainer}>
+                            {language === "pl" && (
+                            <button onClick={() => handleLanguageSwitch("en")}>
+                                <img
+                                    src="https://flagcdn.com/gb.svg" 
+                                    alt="GB Flag"
+                                    className={classes.flagIcon}
+                                />
+                            </button>
+                            )}
+                            {language === "en" && (
+                            <button onClick={() => handleLanguageSwitch("pl")}>
+                                <img
+                                    src="https://flagcdn.com/pl.svg" 
+                                    alt="PL Flag"
+                                    className={classes.flagIcon}
+                                />
+                            </button>
+                            )}
+                        </div>
                     </div>
-                    <div className={classes.languageContainer}>
-                        {language === "pl" && (
-                        <button onClick={() => handleLanguageSwitch("en")}>
-                            <img
-                                src="https://flagcdn.com/gb.svg" 
-                                alt="GB Flag"
-                                className={classes.flagIcon}
-                            />
-                        </button>
-                        )}
-                        {language === "en" && (
-                        <button onClick={() => handleLanguageSwitch("pl")}>
-                            <img
-                                src="https://flagcdn.com/pl.svg" 
-                                alt="PL Flag"
-                                className={classes.flagIcon}
-                            />
-                        </button>
-                        )}
-                    </div>
+                    {!isAddForm && <AddButton
+                        onOptionsClick={secondPageEnable}
+                        isAddForm={isAddForm}
+                    />}
+                    {isAddForm && <SearchButton
+                        onOptionsClick={secondPageEnable}
+                        isAddForm={isAddForm}
+                    />}
+                    {!token && <LoginButton />}
+                    {token &&<Bars onModalActivation={modalHandler}/>}
                 </div>
-                {!isAddForm && <AddButton
-                    onOptionsClick={secondPageEnable}
-                    isAddForm={isAddForm}
-                />}
-                {isAddForm && <SearchButton
-                    onOptionsClick={secondPageEnable}
-                    isAddForm={isAddForm}
-                />}
-                {!token && <LoginButton />}
-                {token && <MyPostsButton/>}
-                {token && <LogoutButton/>}
             </div>
+            {modal && 
+            <div className={classes.modal_container}>
+                <div onClick={modalDisabler} className={classes.background}></div>
+                <div className={classes.modal}>
+                    <MyPostsButton/>
+                    <LogoutButton/>
+                </div>
+            </div>}
         </div>
     )
 };
